150f3aee61cef29b0b40234ff858440f09796fd0
niuzigecc <103304281+niuzigecc@users.noreply.github.com>
2023-03-05 21:58:30 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -33,7 +33,7 @@ const USER_CONFIG = {
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '狗狗', year: '2001', date: '10-05',
+          type: '生日', name: '狗狗', year: '2000', date: '11-14',
         },
         {
           type: '节日', name: '相识纪念日', year: '2017', date: '01-16',
