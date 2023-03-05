87853dafe7a421e8abca7986798a58fdfda225a5
niuzigecc <103304281+niuzigecc@users.noreply.github.com>
2023-03-05 21:51:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'olURh5-sl63biyE_GsjzcsuowSKs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      usetemplate_id: 'olURh5-sl63biyE_GsjzcsuowSKs',
+      useTemplateId: 'mdDZXHfSF8sLSBW8zNcWBSuR3d9lNxDNjaDTa8gVM_s',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-14',
       festivals: [
